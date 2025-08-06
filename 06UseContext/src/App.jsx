a22980e1e999ea70c2afd32907fd5cc78e846c46
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-08-07 04:23:20 +0500

    feat: initialize UserContext implementation

---
+++ 06UseContext/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>useContext , provider topic</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
