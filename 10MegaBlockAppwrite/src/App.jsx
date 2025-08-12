42a37ecf9ca189fd83a9618aad9cb7fb3bc50305
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-08-12 23:27:07 +0500

    feat: initialize 10MegaBlockAppwrite project. - Configured environment variables for Appwrite settings.

---
+++ 10MegaBlockAppwrite/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+    <h1>BlogApp</h1>
+    </>
+  )
+}
+
+export default App
