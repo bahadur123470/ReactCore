8c5d8c07a0870e44d7d880de862400663606a38e
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-08-15 05:42:19 +0500

    refactor: Simplify authentication logic and improve code readability

--- 10MegaBlockAppwrite/src/App.jsx
+++ 10MegaBlockAppwrite/src/App.jsx
@@ -1,9 +1,10 @@
+
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
-import authService from './appwrite/auth'
-import { login, logout } from './store/auth/authSlice'
-import { Header, Footer } from './components/index'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
 
 function App() {
@@ -11,33 +12,25 @@ function App() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const checkAuthStatus = async () => {
-      try {
-        const userData = await authService.getCurrentUser();
-        if (userData) {
-          dispatch(login({ userData }));
-        } else {
-          dispatch(logout());
-        }
-      } catch (error) {
-        console.log("Auth check failed:", error);
-        dispatch(logout());
-      } finally {
-        setLoading(false);
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
       }
-    };
-    
-    checkAuthStatus();
-  }, [dispatch])
-
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
-        <Header/>
+        <Header />
         <main>
-          <Outlet />
+        TODO:  <Outlet />
         </main>
-        <Footer/>
+        <Footer />
       </div>
     </div>
   ) : null
